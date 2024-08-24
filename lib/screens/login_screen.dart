5e901e7348e01311d4c30a397394dcd9b04bf180
anishtupe <tupeanish31@gmail.com>
2024-08-24 17:48:25 +0530

    added screens folder

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _loginscreen extends StatefulWidget {
+  const _loginscreen({super.key});
+
+  @override
+  State<_loginscreen> createState() => __loginscreenState();
+}
+
+class __loginscreenState extends State<_loginscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
