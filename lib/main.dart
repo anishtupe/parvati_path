2c1bce14dc9f654e1f4e6807746b6bcd3cca9bd1
anishtupe <tupeanish31@gmail.com>
2024-08-26 22:30:15 +0530

    made changes to text in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'no of times ive messed  up',
             ),
             Text(
               '$_counter',
